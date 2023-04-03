10dc23ff2043c365836c2e39b9dd1384e5c8fc0c
MotherOfCat00 <asiakotwica245@gmail.com>
2023-04-03 21:20:54 +0200

    Zajęcia

--- app/src/main/java/com/example/examplefirestore/MainActivity.kt
+++ app/src/main/java/com/example/examplefirestore/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.examplefirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.examplefirestore.databinding.ActivityMainBinding
 
 const val TAG = "FIRESTORE"
@@ -13,5 +14,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        uploadData()
+    }
+
+    private fun uploadData() {
+        binding!!.btnUploadData.setOnClickListener{
+
+            val hashMap = hashMapOf<String, Any>(
+                "name" to "Janko Muzykant",
+                "city" to "Szczecin",
+                "age" to 24
+            )
+
+            FirebaseUtils().fireStoreDatabase.collection("users")
+                .add(hashMap)
+                .addOnSuccessListener {
+                    Log.d(TAG, "OK")
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "$exception")
+                }
+        }
     }
 }
